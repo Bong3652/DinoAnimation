97220c1bffd87b620f9e2375d49e529f041f658d
Yong Dong <yongdong11467@gmail.com>
2018-11-01 21:01:17 -0400

    New Directory

--- src/run.java
+++ src/run.java
@@ -1,6 +1,6 @@
 public class run {
     public static void main(String[] args) {
         Dino d = new Dino();
-        d.drawDino("/Users/apple/Downloads/Animation?/src/TRex");
+        d.drawDino("TextFiles/TRex");
     }
 }
