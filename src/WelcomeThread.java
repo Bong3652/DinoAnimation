f35f371394813f3c0fe64f85921569ab03164747
Yong Dong <yongdong11467@gmail.com>
2018-11-01 21:29:07 -0400

    Crappy Thread

---
+++ src/WelcomeThread.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WelcomeThread implements Runnable {
+    @Override
+    public void run() {
+        File dinoFile = new File("TextFiles/Welcome");
+        try {
+            BufferedReader readingDino = new BufferedReader(new FileReader(dinoFile));
+            if (!dinoFile.exists()) {
+                System.out.println("No such Dino");
+            }
+            String line = readingDino.readLine();
+            while (line != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    System.out.print(line.charAt(i));
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException welcomeSleep) {
+                        System.out.println("Welcome Sleeping");
+                    }
+                }
+                System.out.println();
+                line = readingDino.readLine();
+            }
+            System.out.println();
+        } catch (IOException e) {
+            System.out.println("You did wrong");
+        }
+    }
+}
--- src/run.java
+++ src/run.java
@@ -1,6 +1,12 @@
 public class run {
-    public static void main(String[] args) {
+    public static void start() {
         Dino d = new Dino();
+        WelcomeThread wt = new WelcomeThread();
+        wt.run();
         d.drawDino("TextFiles/TRex");
+        d.drawDino("TextFiles/Ground");
+    }
+    public static void main(String[] args) {
+        start();
     }
 }
